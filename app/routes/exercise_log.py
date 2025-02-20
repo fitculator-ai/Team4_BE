92f3e281a72bd4690fc4a374659d399fe42425f8
GordPark <gord23p@gmail.com>
2025-02-20 12:05:53 +0900

    File path update

--- app/routes/exercise_log.py
+++ app/routes/exercise_log.py
@@ -1,6 +1,6 @@
 from app.schemas import ExerciseLogCreate, ExerciseLogResponse, ExerciseLogUpdate, DeleteResponse, ExerciseLogView
-from utils.db_operations import exercise_log_format, exercise_log_delete
-from utils.utils import get_exercise_logs,get_user_info,exercise_intensity
+from app.utils.db_operations import exercise_log_format, exercise_log_delete
+from app.utils.utils import get_exercise_logs,get_user_info,exercise_intensity
 from fastapi import APIRouter, Depends, HTTPException
 from app.models import ExerciseLog
 from sqlalchemy.orm import Session
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from fastapi import FastAPI, Depends, HTTPException
 from datetime import timedelta, datetime
-from config import SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM
+from app.config import SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM
 from jose import jwt
 
 
