ca6d3d65b7491b2720f36f6809f32bab05070313
이은찬 <ieunchan@ieunchan-ui-MacBookAir.local>
2025-02-25 10:42:24 +0900

    fix: exercise-list include exercise.id

--- app/routes/exercise.py
+++ app/routes/exercise.py
@@ -7,14 +7,14 @@ from typing import List, Dict
 router = APIRouter()
 
 # 운동 리스트 조회 API
-@router.get("/", response_model=Dict[str, List[str]], summary="유산소, 근력 운동들을 불러옴")
+@router.get("/",  summary="유산소, 근력 운동들을 불러옴")
 def get_exercise(db: Session = Depends(get_db)):
 
-    cardio_ex = db.query(Exercise.exercise_name).filter(Exercise.exercise_type == ExerciseTypeEnum.Cardio).all()
-    strength_ex = db.query(Exercise.exercise_name).filter(Exercise.exercise_type == ExerciseTypeEnum.Strength).all()
+    cardio_exercise = db.query(Exercise.exercise_name, Exercise.id).filter(Exercise.exercise_type == ExerciseTypeEnum.Cardio).all()
+    strength_exercise = db.query(Exercise.exercise_name, Exercise.id).filter(Exercise.exercise_type == ExerciseTypeEnum.Strength).all()
 
-    cardio_list = [exercise.exercise_name for exercise in cardio_ex]
-    strength_list = [exercise.exercise_name for exercise in strength_ex]
+    cardio_list = [{"name" : exercise.exercise_name, "id" : exercise.id} for exercise in cardio_exercise]
+    strength_list = [{"name" : exercise.exercise_name, "id" : exercise.id} for exercise in strength_exercise]
 
     exercise_list = {
         "유산소": cardio_list,
